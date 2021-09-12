7131aa42f9e63e9302c04f5e4420ca24274ac9d8
Stephanie Hill <shill77@hydra0.eecs.utk.edu>
2021-09-12 17:08:52 -0400

    a few of the functions should be working now. still working

--- Files/merge.cpp
+++ Files/merge.cpp
@@ -16,11 +16,55 @@ void merge_sort(List &l, bool numeric) {
 }
 
 Node *msort(Node *head, bool numeric) {
+	Node* left;
+	Node* right;
+
+	// Handles if list length is <= 1
+	if ((head == NULL) || (head->next == NULL))
+		return;
+
+	split(head, &left, &right); 
+	merge(&left, &right);
+	return &head;
 }
 
+// Splits the list
 void split(Node *head, Node *&left, Node *&right) {
+	
+	Node* slow;
+	Node* fast;
+	slow=head;
+	fast=head->next;
+	
+	// Finds the middle of the list
+	while (fast != NULL){
+		fast = fast->next;
+		if (fast != NULL){
+			slow = slow->next;
+			fast = fast->next;
+		}
+	}
+	// Splits list in two at middle
+	*left = head;
+	*right = slow->next;
+	slow->next = NULL;
 }
 
+// Sorts the list
 Node *merge(Node *left, Node *right, bool numeric) {
+	Node* sorted = NULL;
+
+	// Handles if either node is empty
+	if (left == NULL)
+		return right;
+	else if (b == NULL)
+		return left;
+
+	if(left->data <= right->data){
+		sorted = left;
+		sorted->next = merge(left, right->next);
+	}
+	return sorted;
 }
+
 
