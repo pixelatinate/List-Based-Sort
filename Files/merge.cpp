fcfd79cbe6f4fa27188fedb963da5e98e3912cbf
Stephanie Hill <shill77@hydra0.eecs.utk.edu>
2021-09-13 16:48:50 -0400

    working on functions. still not working

--- Files/merge.cpp
+++ Files/merge.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 
+using namespace std;
 // Prototypes
 
 Node *msort(Node *head, bool numeric);
@@ -12,20 +13,26 @@ Node *merge(Node *left, Node *right, bool numeric);
 
 // Implementations
 
+// Performs the merge sort algorithm
 void merge_sort(List &l, bool numeric) {
+	msort(l.head, numeric);
 }
 
+// Recursive - Calls split to divide and calls merge to conquer
+// Returns new head
 Node *msort(Node *head, bool numeric) {
 	Node* left;
 	Node* right;
 
 	// Handles if list length is <= 1
 	if ((head == NULL) || (head->next == NULL))
-		return;
-
-	split(head, &left, &right); 
-	merge(&left, &right);
-	return &head;
+		return head;
+	
+	split(head, left, right);
+	left = msort(left, numeric);
+	right = msort(right, numeric);
+	merge(left, right, numeric);
+	return head;
 }
 
 // Splits the list
@@ -45,24 +52,29 @@ void split(Node *head, Node *&left, Node *&right) {
 		}
 	}
 	// Splits list in two at middle
-	*left = head;
-	*right = slow->next;
+	left = head;
+	right = slow->next;
 	slow->next = NULL;
 }
 
-// Sorts the list
+// Combines left and right lists and returns the new head
 Node *merge(Node *left, Node *right, bool numeric) {
 	Node* sorted = NULL;
 
 	// Handles if either node is empty
 	if (left == NULL)
 		return right;
-	else if (b == NULL)
+	else if (right == NULL)
 		return left;
 
-	if(left->data <= right->data){
-		sorted = left;
-		sorted->next = merge(left, right->next);
+	while(left != NULL && right !=NULL){
+		if(left <= right){
+			sorted = left;
+			sorted->next = right;
+		}
+		else
+			sorted = right;
+		sorted->next = left;
 	}
 	return sorted;
 }
