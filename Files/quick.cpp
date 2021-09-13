f1fb512b854ea4116ec573df9166ed5a65c27aef
Carter Royce Earheart-Brown <cearhear@hydra9.eecs.utk.edu>
2021-09-13 12:44:03 -0400

    wrote concatenate helper function

--- Files/quick.cpp
+++ Files/quick.cpp
@@ -1,4 +1,5 @@
 // quick.cpp
+// worked on by Carter Earheart-Brown
 
 #include "volsort.h"
 
@@ -13,6 +14,7 @@ Node *concatenate(Node *left, Node *right);
 // Implementations
 
 void quick_sort(List &l, bool numeric) {
+	
 }
 
 Node *qsort(Node *head, bool numeric) {
@@ -21,6 +23,18 @@ Node *qsort(Node *head, bool numeric) {
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric) {
 }
 
+//joins left and right sublists
 Node *concatenate(Node *left, Node *right) {
+	Node *tmp = left;
+	//cases of left and right sublists being empty
+	if (left == NULL)
+		return right;
+	if (right == NULL)
+		return left;
+	//finds end of first list
+	while(tmp->next != NULL)
+		tmp = tmp->next;
+	//joins last element of left to first element of right
+	tmp->next = right;
+	return left;
 }
-
