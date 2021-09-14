1ed9166c6f13a9b65be6f6c279249398fa1ba065
Carter Royce Earheart-Brown <cearhear@hydra9.eecs.utk.edu>
2021-09-14 13:35:15 -0400

    still working out how to keep track of end of left and right sublists

--- Files/quick.cpp
+++ Files/quick.cpp
@@ -14,46 +14,67 @@ Node *concatenate(Node *left, Node *right);
 // Implementations
 
 void quick_sort(List &l, bool numeric) {
-	
-	//qsort(l.head, numeric);
+	qsort(l.head, numeric);
 }
 
 Node *qsort(Node *head, bool numeric) {
 	Node *pivot = head;
-	Node *tmp;
-	//base case
-	//if(pivot->next == NULL)
-	//	return;
-	//Node *left = NULL;
-	//Node *right = NULL;
+	//base case size 0 or 1 for list
+	if(head == NULL or head->next == NULL)
+		return head;
+	Node *left = NULL;
+	Node *right = NULL;
 	//call partition to divide list
-	//partition(head, pivot, left, right, numeric)
+	partition(head, pivot, left, right, numeric);
 	//need to add pivot to one of the lists before concatenate
-	//concatenate(left, right);
+	concatenate(left, right);
 	//qsort(left half, numeric);
 	//qsort(right half, numeric);
 }
 
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric) {
 	Node *current = pivot->next;
+	Node *tmp;
 	Node *leftHead, *rightHead;
 	
 	//looking through list
 	while(current->next != NULL){
 		if(numeric){
 			if(node_number_compare(pivot, current)){
-				left->next = current;
-			} else
-				right->next = current;
+				if(left == NULL){
+					leftHead = current;
+					left = current;
+				} else
+					left->next = current;
+			} else {
+			 	if(right == NULL){
+					rightHead = current;
+					right = current;
+				} else {
+					right->next = current;
+				}
+			}
 		} else {
-			if(node_string_compare(pivot, current))
-				left->next = current;
-			else
-				right->next = current;
+			if(node_string_compare(pivot, current)){
+				if(left == NULL){
+					leftHead = current;
+					left = current;
+				} else
+					left->next = current;
+			} else {
+			 	if(right == NULL){
+					rightHead = current;
+					right = current;
+				} else {
+					right->next = current;
+				}
+			}
 		}
-		current = current->next;		
-	}
+		//increment
+		current = current->next;
 
+	}
+	//update left and right sublists so pivot is in place
 		
 }
 
